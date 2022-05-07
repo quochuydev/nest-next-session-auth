28ebb11cf32232e201df3e9a20e7349349198de6
quochuydev <quochuy.dev@gmail.com>
2022-05-07 22:16:16 +0700

    update

--- web/components/LoginForm/index.tsx
+++ web/components/LoginForm/index.tsx
@@ -45,7 +45,7 @@ function LoginForm({ onClose, onSuccess }: any): React.ReactElement {
         footer={
           <>
             <button
-              className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              className="text-gray-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button"
               onClick={onClose}
             >
@@ -53,7 +53,7 @@ function LoginForm({ onClose, onSuccess }: any): React.ReactElement {
             </button>
             <button
               onClick={onLogin}
-              className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+              className="bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
             >
               Login
             </button>
--- web/components/RegisterForm/index.tsx
+++ web/components/RegisterForm/index.tsx
@@ -51,7 +51,7 @@ function RegisterForm({ onClose }: { onClose: any }): React.ReactElement {
         footer={
           <>
             <button
-              className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              className="text-gray-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button"
               onClick={onClose}
             >
@@ -59,7 +59,7 @@ function RegisterForm({ onClose }: { onClose: any }): React.ReactElement {
             </button>
             <button
               onClick={onRegister}
-              className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              className="bg-gray-500 text-white active:bg-gray-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             >
               Register
             </button>
